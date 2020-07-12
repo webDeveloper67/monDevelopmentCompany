48c6dd1c390c14b06a5d1449f4ee96dc527049d4
mdee <design4web2014@gmail.com>
2020-07-12 15:37:31 +0430

    creating the drawer menu

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -59,9 +59,22 @@ const Header = () => {
   const classes = useStyles();
 
   const [value, setValue] = useState(0);
-
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const menuOptions = [
+    { name: 'Services', link: '/services' },
+    { name: 'Custom Software Development', link: '/customsoftware' },
+    { name: 'Mobile App Development', link: '/mobileapps' },
+    { name: 'Website Development', link: '/websites' }
+  ];
+
+  const handleMenuItemClick = (e, i) => {
+    setAnchorEl(null);
+    setOpen(false);
+    setSelectedIndex(i);
+  };
 
   const handleChange = (e, value) => {
     setValue(value);
@@ -91,6 +104,63 @@ const Header = () => {
         setValue(4);
       } else if (window.location.pathname === '/estimate' && value !== 5) {
         setValue(5);
+      }
+
+      switch (window.location.pathname) {
+        case '/':
+          if (value !== 0) {
+            setValue(0);
+          }
+          break;
+        case '/services':
+          if (value !== 1) {
+            setValue(1);
+            setSelectedIndex(0);
+          }
+          break;
+        case '/customsoftware':
+          if (value !== 1) {
+            setValue(1);
+            setSelectedIndex(1);
+          }
+          break;
+        case '/mobileapps':
+          if (value !== 1) {
+            setValue(1);
+            setSelectedIndex(2);
+          }
+          break;
+        case '/websites':
+          if (value !== 1) setValue(1);
+          setSelectedIndex(3);
+
+          break;
+        case '/revolution':
+          if (value !== 2) {
+            setValue(2);
+          }
+          break;
+
+        case '/about':
+          if (value !== 3) {
+            setValue(3);
+          }
+          break;
+
+        case '/contact':
+          if (value !== 4) {
+            setValue(4);
+          }
+          break;
+
+        case '/estimate':
+          if (value !== 5) {
+            setValue(5);
+          }
+          break;
+
+        default:
+          break;
       }
     },
     [value]
@@ -164,50 +234,22 @@ const Header = () => {
             MenuListProps={{ onMouseLeave: handleClose }}
             elevation={0}
           >
-            <MenuItem
-              onClick={() => {
-                handleClose();
-                setValue(1);
-              }}
-              component={Link}
-              to="/services"
-              classes={{ root: classes.menuItem }}
-            >
-              Services
-            </MenuItem>
-            <MenuItem
-              onClick={() => {
-                handleClose();
-                setValue(1);
-              }}
-              component={Link}
-              to="/customsoftware"
-              classes={{ root: classes.menuItem }}
-            >
-              Custom Software Development
-            </MenuItem>
-            <MenuItem
-              onClick={() => {
-                handleClose();
-                setValue(1);
-              }}
-              component={Link}
-              to="/mobileapps"
-              classes={{ root: classes.menuItem }}
-            >
-              Mobile App Development
-            </MenuItem>
-            <MenuItem
-              onClick={() => {
-                handleClose();
-                setValue(1);
-              }}
-              component={Link}
-              to="/websites"
-              classes={{ root: classes.menuItem }}
-            >
-              Website Development
-            </MenuItem>
+            {menuOptions.map((option, i) =>
+              <MenuItem
+                key={option}
+                component={Link}
+                to={option.link}
+                classes={{ root: classes.menuItem }}
+                onClick={e => {
+                  handleMenuItemClick(e, i);
+                  setValue(1);
+                  handleClose();
+                }}
+                selected={i === selectedIndex}
+              >
+                {option.name}
+              </MenuItem>
+            )}
           </Menu>
         </Toolbar>
       </AppBar>
