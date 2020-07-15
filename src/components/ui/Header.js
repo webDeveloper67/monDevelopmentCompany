e840c06f0d523dac15419892901b178e163af73c
mdee <design4web2014@gmail.com>
2020-07-15 22:22:55 +0430

    finishing header component along with drawer for mobile devices

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -13,6 +13,9 @@ import { useTheme } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 // Images
 import logo from '../../assets/logo.svg';
@@ -79,6 +82,20 @@ const useStyles = makeStyles(theme => ({
     '&:hover': {
       backgroundColor: 'transparent'
     }
+  },
+  drawer: {
+    backgroundColor: theme.palette.common.blue
+  },
+  drawerItem: {
+    ...theme.typography.tab,
+    color: 'white',
+    opacity: 0.7
+  },
+  drawerItemEstimate: {
+    backgroundColor: theme.palette.common.orange
+  },
+  drawerItemSelected: {
+    opacity: 1
   }
 }));
 
@@ -96,10 +113,40 @@ const Header = () => {
   const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
 
   const menuOptions = [
-    { name: 'Services', link: '/services' },
-    { name: 'Custom Software Development', link: '/customsoftware' },
-    { name: 'Mobile App Development', link: '/mobileapps' },
-    { name: 'Website Development', link: '/websites' }
+    { name: 'Services', link: '/services', activeIndex: 1, selectedIndex: 0 },
+    {
+      name: 'Custom Software Development',
+      link: '/customsoftware',
+      activeIndex: 1,
+      selectedIndex: 1
+    },
+    {
+      name: 'Mobile App Development',
+      link: '/mobileapps',
+      activeIndex: 1,
+      selectedIndex: 2
+    },
+    {
+      name: 'Website Development',
+      link: '/websites',
+      activeIndex: 1,
+      selectedIndex: 3
+    }
+  ];
+
+  const routes = [
+    { name: 'Home', link: '/', activeIndex: 0 },
+    {
+      name: 'Services',
+      link: '/services',
+      activeIndex: 1,
+      ariaOwns: anchorEl ? 'simple-menu' : undefined,
+      ariaPopup: anchorEl ? 'true' : undefined,
+      mouseOver: e => handleClick(e)
+    },
+    { name: 'The Revolution', link: '/revolution', activeIndex: 2 },
+    { name: 'About Us', link: '/about', activeIndex: 3 },
+    { name: 'Contact Us', link: '/contact', activeIndex: 4 }
   ];
 
   const handleMenuItemClick = (e, i) => {
@@ -124,78 +171,22 @@ const Header = () => {
 
   useEffect(
     () => {
-      if (window.location.pathname === '/' && value !== 0) {
-        setValue(0);
-      } else if (window.location.pathname === '/services' && value !== 1) {
-        setValue(1);
-      } else if (window.location.pathname === '/revolution' && value !== 2) {
-        setValue(2);
-      } else if (window.location.pathname === '/about' && value !== 3) {
-        setValue(3);
-      } else if (window.location.pathname === '/contact' && value !== 4) {
-        setValue(4);
-      } else if (window.location.pathname === '/estimate' && value !== 5) {
-        setValue(5);
-      }
-
-      switch (window.location.pathname) {
-        case '/':
-          if (value !== 0) {
-            setValue(0);
-          }
-          break;
-        case '/services':
-          if (value !== 1) {
-            setValue(1);
-            setSelectedIndex(0);
-          }
-          break;
-        case '/customsoftware':
-          if (value !== 1) {
-            setValue(1);
-            setSelectedIndex(1);
-          }
-          break;
-        case '/mobileapps':
-          if (value !== 1) {
-            setValue(1);
-            setSelectedIndex(2);
-          }
-          break;
-        case '/websites':
-          if (value !== 1) setValue(1);
-          setSelectedIndex(3);
-
-          break;
-        case '/revolution':
-          if (value !== 2) {
-            setValue(2);
-          }
-          break;
-
-        case '/about':
-          if (value !== 3) {
-            setValue(3);
-          }
-          break;
-
-        case '/contact':
-          if (value !== 4) {
-            setValue(4);
-          }
-          break;
-
-        case '/estimate':
-          if (value !== 5) {
-            setValue(5);
-          }
-          break;
-
-        default:
-          break;
-      }
+      [...menuOptions, ...routes].forEach(route => {
+        switch (window.location.pathname) {
+          case `${route.link}`:
+            if (value !== route.activeIndex) {
+              setValue(route.activeIndex);
+              if (route.activeIndex && route.selectedIndex !== selectedIndex) {
+                setSelectedIndex(route.selectedIndex);
+              }
+            }
+            break;
+          default:
+            break;
+        }
+      });
     },
-    [value]
+    [menuOptions, routes, selectedIndex, value]
   );
 
   const tabs = (
@@ -206,34 +197,18 @@ const Header = () => {
         className={classes.tabContainer}
         indicatorColor="primary"
       >
-        <Tab className={classes.tab} component={Link} to="/" label="Home" />
-        <Tab
-          aria-owns={anchorEl ? 'simple-menu' : undefined}
-          aria-haspopup={anchorEl ? 'true' : undefined}
-          className={classes.tab}
-          component={Link}
-          to="/services"
-          label="Services"
-          onMouseOver={e => handleClick(e)}
-        />
-        <Tab
-          className={classes.tab}
-          component={Link}
-          to="/revolution"
-          label="The Revolution"
-        />
-        <Tab
-          className={classes.tab}
-          component={Link}
-          to="/about"
-          label="About Us"
-        />
-        <Tab
-          className={classes.tab}
-          component={Link}
-          to="/contact"
-          label="Contact Us"
-        />
+        {routes.map((route, index) =>
+          <Tab
+            key={`${route}${index}`}
+            className={classes.tab}
+            component={Link}
+            to={route.link}
+            label={route.name}
+            aria-owns={route.ariaOwns}
+            aria-haspopup={route.ariaPopup}
+            onMouseOver={route.mouseOver}
+          />
+        )}
       </Tabs>
       <Button variant="contained" color="secondary" className={classes.button}>
         Free Estimate
@@ -275,8 +250,58 @@ const Header = () => {
         open={openDrawer}
         onClose={() => setOpenDrawer(false)}
         onOpen={() => setOpenDrawer(true)}
+        classes={{ paper: classes.drawer }}
       >
-        example Drawer
+        <List disablePadding>
+          {routes.map(route =>
+            <ListItem
+              key={`${route}${route.activeIndex}`}
+              divider
+              button
+              component={Link}
+              to={route.link}
+              selected={value === route.activeIndex}
+              onClick={() => {
+                setOpenDrawer(false);
+                setValue(route.activeIndex);
+              }}
+            >
+              <ListItemText
+                className={
+                  value === route.activeIndex
+                    ? [classes.drawerItem, classes.drawerItemSelected]
+                    : classes.drawerItem
+                }
+                disableTypography
+              >
+                {route.name}
+              </ListItemText>
+            </ListItem>
+          )}
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false);
+              setValue(5);
+            }}
+            divider
+            button
+            component={Link}
+            to="/estimate"
+            selected={value === 5}
+            className={classes.drawerItemEstimate}
+          >
+            <ListItemText
+              className={
+                value === 5
+                  ? [classes.drawerItem, classes.drawerItemSelected]
+                  : classes.drawerItem
+              }
+              disableTypography
+            >
+              Free Estimate
+            </ListItemText>
+          </ListItem>
+        </List>
       </SwipeableDrawer>
       <IconButton
         className={classes.drawerIconContainer}
