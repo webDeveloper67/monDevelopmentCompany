ea31295d4887755d239b913806cf37540d08a98e
mdee <design4web2014@gmail.com>
2020-09-06 20:15:13 +0430

    adding mobile apps and services routes

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -10,17 +10,83 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import backArrow from '../assets/backArrow.svg';
 import forwardArrow from '../assets/forwardArrow.svg';
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+  heading: {
+    maxWidth: '40em'
+  },
+  arrowContainer: {
+    marginTop: '0.5em'
+  },
+  mainContainer: {
+    paddingLeft: '5em',
+    paddingRight: '5em',
+    paddingTop: '2em',
+    paddingBottom: '10em'
+  }
+}));
 
-const CustomSoftware = () => {
+const CustomSoftware = ({ setSelectedIndex }) => {
   const classes = useStyles();
 
   return (
-    <Grid container direction="column">
+    <Grid container direction="column" className={classes.mainContainer}>
       <Grid item container direction="row">
-        <Grid item>
-          <IconButton>
+        <Grid
+          item
+          className={classes.arrowContainer}
+          style={{ marginRight: '1em', marginLeft: '-3.5em' }}
+        >
+          <IconButton
+            style={{ backgroundColor: 'transparent' }}
+            component={Link}
+            to="/services"
+            onClick={() => setSelectedIndex(0)}
+          >
             <img src={backArrow} alt="back to services page" />
+          </IconButton>
+        </Grid>
+
+        <Grid item container direction="column" className={classes.heading}>
+          <Grid item>
+            <Typography variant="h2">Custom Software Development</Typography>
+          </Grid>
+
+          <Grid item>
+            <Typography variant="body1">
+              Whether we’re replacing old software or inventing new solutions,
+              Arc Development is here to help your business tackle technology.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Using regular commercial software leaves you with a lot of stuff
+              you don’t need, without some of the stuff you do need, and
+              ultimately controls the way you work. Without using any software
+              at all you risk falling behind competitors and missing out on huge
+              savings from increased efficiency.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Our custom solutions are designed from the ground up with your
+              needs, wants, and goals at the core. This collaborative process
+              produces finely tuned software that is much more effective at
+              improving your workflow and reducing costs than generalized
+              options.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              We create exactly what you what, exactly how you want it.
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid item className={classes.arrowContainer}>
+          <IconButton
+            style={{ backgroundColor: 'transparent' }}
+            component={Link}
+            to="/mobileapps"
+            onClick={() => setSelectedIndex(2)}
+          >
+            <img
+              src={forwardArrow}
+              alt="Forward to iOS/Android App Development Page"
+            />
           </IconButton>
         </Grid>
       </Grid>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -66,6 +66,11 @@ export default createMuiTheme({
       borderRadius: 50,
       fontFamily: 'Roboto',
       fontWeight: 'bold'
+    },
+    body1: {
+      fontSize: '1.25rem',
+      color: monGrey,
+      fontWeight: 300
     }
   }
 });
